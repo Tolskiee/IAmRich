dfc5984d9b52a43a7dfc85bb7c0b337193b39368
Val Jeremy Tolosa <vjeremytolosa@gmail.com>
2022-08-11 23:24:50 +0800

    chore(project): Initialize files and created Material App

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,19 @@ import 'package:flutter/material.dart';
 //main body for entire code edit codes here
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Center(child: Text('I Am Rich!!')),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: const Center(
+          child: Image(
+            image: NetworkImage(
+                'https://i.kym-cdn.com/entries/icons/original/000/005/574/takemymoney.jpg'),
+          ),
+        ),
       ),
     ),
   );
