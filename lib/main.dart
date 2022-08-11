25017a8d5b5ccc4c9c6d3c4e51895ac7e4c7d1d3
Val Jeremy Tolosa <vjeremytolosa@gmail.com>
2022-08-12 01:41:42 +0800

    feat(project): Added images directory for assets

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,12 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: Center(child: Text('I Am Rich!!')),
+          title: const Center(child: Text('I Am Rich!')),
           backgroundColor: Colors.blueGrey[900],
         ),
         body: const Center(
           child: Image(
-            image: NetworkImage(
-                'https://i.kym-cdn.com/entries/icons/original/000/005/574/takemymoney.jpg'),
+            image: AssetImage('images/diamond.png'),
           ),
         ),
       ),
