28477d889c1cfb7772d206d91fa9c6708ce54574
Val Jeremy Tolosa <vjeremytolosa@gmail.com>
2022-08-13 10:54:38 +0800

    feat(project): properly added App icon [working]

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: const Center(child: Text('I Am Rich!')),
+          title: const Center(child: Text('I Am Rich!retest')),
           backgroundColor: Colors.blueGrey[900],
         ),
         body: const Center(
